44b6bd82967b360c50290f628d5686e947a140c5
Rune-of-GJ <kimm91.54@daum.net>
2026-04-05 22:56:27 +0900

    dsaf

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -15,6 +15,26 @@ Rails.application.routes.draw do
   post "/courses/:id/enroll", to: "courses#enroll", as: :enroll_course
 
   namespace :api do
+    scope module: :v1 do
+      post "auth/signup", to: "auth#signup"
+      post "auth/login", to: "auth#login"
+      post "auth/refresh", to: "auth#refresh"
+      post "auth/logout", to: "auth#logout"
+
+      resources :categories, only: [:index, :show]
+      resources :courses, only: [:index, :show] do
+        post :enroll, on: :member
+        resources :lectures, only: [:index], controller: "lectures"
+      end
+
+      resources :lectures, only: [:show] do
+        post :progress, on: :member
+      end
+
+      get "users/me/courses", to: "users#me_courses"
+      get "users/me/progress", to: "users#me_progress"
+    end
+
     namespace :v1 do
       post "auth/signup", to: "auth#signup"
       post "auth/login", to: "auth#login"
